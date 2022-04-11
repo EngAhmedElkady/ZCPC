c1a67fde461b50dc875b5dfad6e90e7346db9f26
EngAhmedElkady <ahelkady110@gmail.com>
2022-04-12 01:44:14 +0200

    .ignore

--- env/lib/python3.7/site-packages/tests/urls.py
+++
@@ -1,9 +0,0 @@
-""" Tests App URL Config """
-from django.conf.urls import include
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path("api/password_reset/", include('django_rest_passwordreset.urls', namespace='password_reset')),
-    path("admin/", admin.site.urls),
-]
