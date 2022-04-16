c2b50ae9e8d89248908b6cb7d6db398451e75811
abdelrahman ibrahem <abdo.ibrahem1122@gmail.com>
2022-04-16 21:06:05 +0200

    add community section

---
+++ communnity/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import DisplayAllCommunity , CreateNewCommunity
+
+urlpatterns = [
+    path('' , DisplayAllCommunity.as_view() , name="all_community"),
+    path('create/' , CreateNewCommunity.as_view() , name="create")
+]
