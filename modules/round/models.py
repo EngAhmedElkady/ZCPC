d2ba9fe1eb6af3e85f3424aaa5cce83c4a340960
EngAhmedElkady <ahelkady110@gmail.com>
2022-04-24 14:30:07 +0200

    Add Round docs

--- modules/round/models.py
+++ modules/round/models.py
@@ -9,7 +9,27 @@ User = get_user_model()
 
 
 class Round(models.Model):
+    """
+    each communnity has many round each round has the following:
+    - team    : like instructor and mentors
+    - student : study in this roudn
 
+    Args:
+        name      : name of this Round
+        disc      : description
+        created_at: the time started at 
+        end_date  : the time ended at 
+        status    : boolean field be False if round ended 
+        
+
+    Returns:
+        team     : function > "return the team for this round"
+        students : function > "return all students study in this round"
+        feedback : function > "return feedback about round and average rate"
+
+    
+        
+    """
     name = models.CharField(max_length=200)
     disc = models.TextField(max_length=700)
     communnity_id = models.ForeignKey(Communnity, on_delete=models.CASCADE)
@@ -32,10 +52,30 @@ class Round(models.Model):
     def members(self):
         "return all members study in this round"
         pass
+    
+    def feedback(self):
+        "return feedback about round and average rate"  
+        pass
+        
 
 
-class RoundTeam(models.Model):
-    "This is a team for this round because each round maybe has different team"
+class TeamMember(models.Model):
+    """
+    This is a team for this round because each round maybe has different team.
+    
+    - member    : like instructor and mentors
+
+    Args:
+        user_id   : member_id (should become at the communnity team)
+        round_id  : the round id
+        role      : role like as instructor or mentor
+       
+        
+
+    Returns:
+        feedback : function > "return feedback about round and average rate"  
+
+    """
 
     fields = [('instructor', 'instructor'), ('mentor', 'mentor')]
 
@@ -49,6 +89,10 @@ class RoundTeam(models.Model):
 
     def str(self):
         return f"{self.name} at {self.round.id} as {self.role}."
+    
+    def feedback(self):
+        "return feedback about round and average rate"  
+        pass
 
 
 class Student(models.Model):
@@ -71,6 +115,8 @@ class Student(models.Model):
 
 
 class RoundFeedback(models.Model):
+    "store the feedback about all members in round team."
+    
     student=models.ForeignKey(Student, on_delete=models.CASCADE)
     round_id = models.ForeignKey(Round, on_delete=models.CASCADE)
     stars = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
@@ -79,12 +125,10 @@ class RoundFeedback(models.Model):
     
 
 class TeamFeedback(models.Model):
+    "store the feedback about all members in round team."
     
-    """store the feedback about all members in round team.
-    
-    """
     student=models.ForeignKey(Student, on_delete=models.CASCADE)
-    team_member = models.ForeignKey(RoundTeam, on_delete=models.CASCADE)
+    team_member = models.ForeignKey(TeamMember, on_delete=models.CASCADE)
     stars = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
     disc=models.TextField()
     
