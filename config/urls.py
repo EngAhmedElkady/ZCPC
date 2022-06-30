a8a16adf3ce5ed1b103539e5ddccfa386f7ccb09
EngAhmedElkady <ahelkady110@gmail.com>
2022-06-30 12:16:48 +0200

    url media

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,11 @@ from rest_framework.schemas import get_schema_view
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf.urls import url
+from django.views.static import serve
+from django.conf import settings
+from django.conf.urls.static import static
+
 schema_view = get_schema_view(
     openapi.Info(
         title="ICPC API",
@@ -49,4 +54,12 @@ urlpatterns = [
         'swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui(
         'redoc', cache_timeout=0), name='schema-redoc'),
+    
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
