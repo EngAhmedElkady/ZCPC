7a226345da02833ace4bed5d405b3d2999d27d74
EngAhmedElkady <ahelkady110@gmail.com>
2022-06-30 12:23:57 +0200

    url media

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS =['127.0.0.1','0.0.0.0','zcpc.herokuapp.com']
 
 
 # Application definition
