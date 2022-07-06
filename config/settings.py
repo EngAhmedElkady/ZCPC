80da2acf5f8284cc597cbeb75d0736445281ed7b
EngAhmedElkady <ahelkady110@gmail.com>
2022-07-06 14:37:30 +0200

    add postgrss

--- config/settings.py
+++ config/settings.py
@@ -136,11 +136,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'mydatabase',
+    'USER': 'mydatabaseuser',
+    'PASSWORD': 'mypassword',
+    'HOST': '127.0.0.1',
+    'PORT': '5432',
+}
 }
 
 
