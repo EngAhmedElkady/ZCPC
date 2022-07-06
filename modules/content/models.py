159b50e56dfb1ab67c3bc7ff54654773c4c0b0d3
EngAhmedElkady <ahelkady110@gmail.com>
2022-07-06 14:10:48 +0200

    add staticfiles

--- modules/content/models.py
+++ modules/content/models.py
@@ -1,14 +1,32 @@
 from django.db import models
+from requests import delete
 from modules.level.models import Level
 # Create your models here.
 
 
+class Content(models.Model):
+    name = models.CharField(max_length=50)
+    level = models.ForeignKey(Level,
+                              related_name='contents',
+                              on_delete=models.CASCADE)
+
+    description = models.TextField(blank=True,null=True)
+
+    def get_community(self):
+        return self.level.get_community()
+
+    def __str__(self):
+        return str(self.name)
+
+
 class File(models.Model):
+    content=models.ForeignKey(Content,related_name='files',on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
     file = models.FileField(upload_to='Files')
 
 
 class Video(models.Model):
+    content=models.ForeignKey(Content,related_name='videos',on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
     url = models.URLField()
 
@@ -18,22 +36,3 @@ class Problem(models.Model):
     url = models.URLField()
 
 
-class Content(models.Model):
-    name = models.CharField(max_length=50)
-    level = models.ForeignKey(Level,
-                              related_name='contents',
-                              on_delete=models.CASCADE)
-    files = models.ManyToManyField(
-        File, related_name='files',blank=True)
-    problems = models.ManyToManyField(
-        Problem, related_name='problems',blank=True)
-    videos = models.ManyToManyField(
-        Video, related_name='videos',blank=True)
-
-    description = models.TextField()
-
-    def get_community(self):
-        return self.level.get_community()
-
-    def __str__(self):
-        return str(self.level.name)
