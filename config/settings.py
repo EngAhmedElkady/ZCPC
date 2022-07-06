34f5dc02ee3bdac2677e85fcae78cf4ec61cae30
EngAhmedElkady <ahelkady110@gmail.com>
2022-07-06 14:26:19 +0200

    add middware

--- config/settings.py
+++ config/settings.py
@@ -102,6 +102,7 @@ ACCOUNT_UNIQUE_EMAIL = True  # new
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- config/urls.py
+++ config/urls.py
@@ -57,7 +57,7 @@ urlpatterns = [
     
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
-
+   
 ]
 
 if settings.DEBUG:
