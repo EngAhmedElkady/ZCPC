3d930bc848efead6dcd01d5472e70fdfc3b385e9
unknown <EngAhmedElkady>
2022-08-06 17:38:44 +0200

    user env

--- config/urls.py
+++ config/urls.py
@@ -1,18 +1,4 @@
-"""config URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
@@ -48,7 +34,7 @@ ICPC Platform systems also involve full automation functions that help communiti
 )
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    # path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("admin/", admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('api-auth', include('rest_framework.urls')),
@@ -59,7 +45,7 @@ urlpatterns = [
     path('', include('modules.content.urls')),
     path('blog/', include('modules.blog.urls')),
 
-   path('swagger/', schema_view.with_ui( # new
+   path('', schema_view.with_ui( # new
     'swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui( # new
     'redoc', cache_timeout=0), name='schema-redoc'),
